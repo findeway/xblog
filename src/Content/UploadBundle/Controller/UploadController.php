c5b271b3da27c774547db0a76004db647d77b5de
findeway <findewayz@gmail.com>
2015-03-02 23:58:32 +0800

    Add code and image modal;

--- src/Content/UploadBundle/Controller/UploadController.php
+++ src/Content/UploadBundle/Controller/UploadController.php
@@ -6,9 +6,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class UploadController extends Controller
 {
-    public function uploadImageAction($name)
+    public function uploadImageAction()
     {
-        
+        $file = $this->getRequest()->files->get('uploadedImage');
         return $this->render('ContentUploadBundle:Default:index.html.twig', array('name' => $name));
     }
 }
