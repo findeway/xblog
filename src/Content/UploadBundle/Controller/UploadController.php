4d487de5a95fa76faf181b3d1f38940c9a3278c7
findeway <findewayz@gmail.com>
2015-03-03 19:40:30 +0800

    Add image uploading feature;

--- src/Content/UploadBundle/Controller/UploadController.php
+++ src/Content/UploadBundle/Controller/UploadController.php
@@ -3,12 +3,50 @@
 namespace Content\UploadBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Content\UploadBundle\Entity\UploadImage;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\File\File;
 
 class UploadController extends Controller
 {
-    public function uploadImageAction()
+    public function uploadImageAction(Request $request)
     {
-        $file = $this->getRequest()->files->get('uploadedImage');
-        return $this->render('ContentUploadBundle:Default:upload.html.twig', array('name' => $file));
+        $file = $request->files->get('uploadedImage');
+        if($file instanceof UploadedFile){
+            $uploadImage = new UploadImage($file);
+            $repository = $this->getDoctrine()->getRepository('ContentUploadBundle:UploadImage');
+            $image = $repository->findOneByName($uploadImage->getName());
+            if(!$image){
+                $uploadImage->setUrl($this->generateUrl('content_show_image',array('fileName' => $uploadImage->getFilepath())));
+                $em = $this->getDoctrine()->getManager();
+                $em->persist($uploadImage);
+                $em->flush();
+                $uploadImage->upload();
+                return new Response($uploadImage->getUrl());
+            }else{
+                return new Response($image->getUrl());
+            }
+        }
+        return new Response("Upload Failed",500);
+    }
+    
+    public function showImageAction($fileName)
+    {
+        $repository = $this->getDoctrine()->getRepository('ContentUploadBundle:UploadImage');
+        $name = pathinfo($fileName,PATHINFO_FILENAME);
+        $image = $repository->findOneByName($name);
+        if($image){
+            $fp = fopen($image->getWebPath(), "rb");
+    		$filecontent = stream_get_contents($fp);
+    		fclose($fp);
+    		$response = new Response($filecontent);
+    		$file = new File($image->getWebPath());
+    		$response->headers->set('Content-Type', $file->getMimeType());
+    		return $response;
+        }else{
+            return new Response("File Not Found",404);
+        }
     }
 }
---
+++ src/Content/UploadBundle/Entity/UploadImage.php
@@ -0,0 +1,280 @@
+<?php
+
+namespace Content\UploadBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * Image
+ *
+ * @ORM\Table()
+ * @ORM\Entity
+ */
+class UploadImage
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="filepath", type="string", length=255)
+     */
+    private $filepath;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="uploadDateTime", type="datetime")
+     */
+    private $uploadDateTime;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="url", type="string", length=255)
+     */
+    private $url;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return Image
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set filepath
+     *
+     * @param string $filepath
+     * @return Image
+     */
+    public function setFilepath($filepath)
+    {
+        $this->filepath = $filepath;
+
+        return $this;
+    }
+
+    /**
+     * Get filepath
+     *
+     * @return string 
+     */
+    public function getFilepath()
+    {
+        return $this->filepath;
+    }
+
+    /**
+     * Set uploadDateTime
+     *
+     * @param \DateTime $uploadDateTime
+     * @return Image
+     */
+    public function setUploadDateTime($uploadDateTime)
+    {
+        $this->uploadDateTime = $uploadDateTime;
+
+        return $this;
+    }
+
+    /**
+     * Get uploadDateTime
+     *
+     * @return \DateTime 
+     */
+    public function getUploadDateTime()
+    {
+        return $this->uploadDateTime;
+    }
+
+    /**
+     * Set url
+     *
+     * @param string $url
+     * @return Image
+     */
+    public function setUrl($url)
+    {
+        $this->url = $url;
+
+        return $this;
+    }
+
+    /**
+     * Get url
+     *
+     * @return string 
+     */
+    public function getUrl()
+    {
+        return $this->url;
+    }
+    
+    /**
+     * @Assert\File(
+     *     maxSize = "5M",
+     *     mimeTypes = {"image/jpeg", "image/gif", "image/png", "image/tiff"},
+     *     maxSizeMessage = "The maxmimum allowed file size is 5MB.",
+     *     mimeTypesMessage = "Only the filetypes image are allowed."
+     * )    
+     */
+    private $file;
+
+    private $tempPath;
+    
+    /**
+     * Sets file.
+     *
+     * @param UploadedFile $file
+     */
+    public function setFile(UploadedFile $file = null)
+    {
+        $this->file = $file;
+        // check if we have an old image path
+        if (isset($this->filepath)) {
+            // store the old name to delete after the update
+            $this->tempPath = $this->filepath;
+            $this->filepath = null;
+        } else {
+            $this->filepath = '';
+        }
+        if (null !== $this->getFile()) {
+            // do whatever you want to generate a unique name
+            $filename = md5_file($this->getFile()->getPathname());
+            $this->setName($filename);
+            $this->setFilepath($filename.'.'.$this->getFile()->getClientOriginalExtension());
+        }
+    }
+    
+    /**
+     * Get file.
+     *
+     * @return UploadedFile
+     */
+    public function getFile()
+    {
+        return $this->file;
+    }
+    
+    public function getAbsolutePath()
+    {
+        return null === $this->path
+        ? null
+        : $this->getUploadRootDir().'/'.$this->filepath;
+    }
+    
+    public function getWebPath()
+    {
+        return null === $this->filepath
+        ? null
+        : $this->getUploadDir().'/'.$this->filepath;
+    }
+    
+    protected function getUploadRootDir()
+    {
+        // the absolute directory path where uploaded
+        // documents should be saved
+        return __DIR__.'/../../../../web/'.$this->getUploadDir();
+    }
+    
+    protected function getUploadDir()
+    {
+        // get rid of the __DIR__ so it doesn't screw up
+        // when displaying uploaded doc/image in the view.
+        return 'uploads/documents';
+    }
+
+    /**
+     * @ORM\PrePersist()
+     * @ORM\PreUpdate()
+     */
+    public function preUpload()
+    {
+    }
+    
+    /**
+     * @ORM\PostPersist()
+     * @ORM\PostUpdate()
+     */
+    public function upload()
+    {
+        if (null === $this->getFile()) {
+            return;
+        }
+        // if there is an error when moving the file, an exception will
+        // be automatically thrown by move(). This will properly prevent
+        // the entity from being persisted to the database on error
+        $this->getFile()->move($this->getUploadRootDir(), $this->getFilepath());
+    
+        // check if we have an old image
+        if (isset($this->tempPath)) {
+            // delete the old image
+            unlink($this->getUploadRootDir().'/'.$this->tempPath);
+            // clear the temp image path
+            $this->tempPath = null;
+        }
+        $this->file = null;
+    }
+    
+    /**
+     * @ORM\PostRemove()
+     */
+    public function removeUpload()
+    {
+        if ($file = $this->getAbsolutePath()) {
+            unlink($file);
+        }
+    }
+    
+    function __construct($file = null)
+    {
+        $this->name = '';
+        $this->url = '';
+        $this->uploadDateTime = new \DateTime();
+        $this->setFile($file);
+    }
+}
