2167b4cbacd67c1d522228d5008cf7ab18023800
findeway <findewayz@gmail.com>
2015-03-04 19:28:43 +0800

    Add pagination;

--- src/Content/ArticleBundle/Controller/ArticleController.php
+++ src/Content/ArticleBundle/Controller/ArticleController.php
@@ -4,9 +4,28 @@ namespace Content\ArticleBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Content\ArticleBundle\Entity\Article;
+use Doctrine\ORM\Tools\Pagination\Paginator;
 
 class ArticleController extends Controller
 {
+    //pagination tools;
+    public function getTotalCount()
+    {
+        $repository = $this->getDoctrine()->getRepository('ContentArticleBundle:Article');
+        $totalCount = $repository->createQueryBuilder('u')
+        ->select('count(u.id)')
+        ->getQuery()
+        ->getSingleScalarResult();
+        return $totalCount;
+    }
+    
+    public function getPageCount($pageSize)
+    {
+        $totalCount = $this->getTotalCount();
+        $pageCount = floor($totalCount/$pageSize);
+        return $pageCount;
+    }
+    
     public function indexAction($name)
     {
         return $this->render('ContentArticleBundle:Article:index.html.twig', array('name' => $name));
@@ -101,28 +120,68 @@ class ArticleController extends Controller
     
     public function searchAction()
     {
+        $page = $this->getRequest()->query->get('page');
+        if(empty($page)){
+            $page = 0;
+        }
+        $pageSize = $this->getRequest()->query->get('pagesize');
+        if(empty($pageSize)){
+            $pageSize = 100;
+        }
+        $pageCount = $this->getPageCount($pageSize);
+
         $keywords = $this->getRequest()->query->get('keywords');
+        
         $repository = $this->getDoctrine()->getRepository('ContentArticleBundle:Article');
         $query = $repository->createQueryBuilder('a')->where('a.title like :keywords')
                     ->setParameter('keywords','%'.$keywords.'%')
                     ->orderBy('a.updateDateTime','ASC')
-                    ->getQuery();
+                    ->getQuery()
+                    ->setFirstResult($page*$pageSize)
+                    ->setMaxResults($pageSize);
         $articlesResult = $query->getResult();
-        return $this->render('ContentArticleBundle:Article:article.list.html.twig',array('articles' => $articlesResult));
+        $paginationBaseUrl = $this->generateUrl('content_articles_search').'?keywords='.$keywords.'&pagesize='.$pageSize.'&page=';
+        return $this->render('ContentArticleBundle:Article:article.list.html.twig',array(
+            'articles' => $articlesResult,
+            'page' => $page,
+            'pageSize' => $pageSize,
+            'pageCount' => $pageCount,
+            'pageBaseUrl' => $paginationBaseUrl
+        ));
     }
     
     public function tagAction($tag)
     {
+        $page = $this->getRequest()->query->get('page');
+        if(empty($page)){
+            $page = 0;
+        }
+        $pageSize = $this->getRequest()->query->get('pagesize');
+        if(empty($pageSize)){
+            $pageSize = 100;
+        }
+        $pageCount = $this->getPageCount($pageSize);
+        
         $repository = $this->getDoctrine()->getRepository('ContentArticleBundle:Article');
         if(empty($tag)){
-            $articlesResult = $repository->findAll();        
-        }else{
-            $query = $repository->createQueryBuilder('a')->where('a.tags like :tag')
-                    ->setParameter('tag','%'.$tag.'%')
-                    ->orderBy('a.updateDateTime','ASC')
-                    ->getQuery();
-            $articlesResult = $query->getResult();
+            $tag = '';       
         }
-        return $this->render('ContentArticleBundle:Article:article.list.html.twig',array('articles' => $articlesResult));
+        $query = $repository->createQueryBuilder('a')->where('a.tags like :tag')
+                ->setParameter('tag','%'.$tag.'%')
+                ->orderBy('a.updateDateTime','ASC')
+                ->getQuery()
+                ->setFirstResult($page*$pageSize)
+                ->setMaxResults($pageSize);
+        $articlesResult = $query->getResult();
+        
+        $paginationBaseUrl = $this->generateUrl('content_articles_tag',array('tag' => $tag)).'&pagesize='.$pageSize.'&page=';
+        return $this->render('ContentArticleBundle:Article:article.list.html.twig',array(
+            'articles' => $articlesResult,
+            'page' => $page,
+            'pageSize' => $pageSize,
+            'pageCount' => $pageCount,
+            'pageBaseUrl' => $paginationBaseUrl
+        ));
     }
+   
 }
